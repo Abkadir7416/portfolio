e81afc975d87f581dd686b0ea3501fd15e8a9f72
Abdul <abdulk0749@gmail.com>
2024-12-26 23:31:12 +0530

    modification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <div className="mx-2 w-15 ">
-          <span className="text-3xl">Abdul</span>
+          <span className="text-3xl">A-K</span>
         </div>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
